9ff41ffa5da5c0d717c7af441a8a35ba4032911e
wowyunDBL <wowyunpg@gmail.com>
2021-07-27 14:43:30 +0800

    0727

--- backup.sh
+++ backup.sh
@@ -2,6 +2,9 @@ pip install -r requirements.txt
 mkdir -p ~/cpp
 # and then put put_in_cpp.zip
 
+mkdir -p ~/shapefiles/test/pos
+mkdir -p ~/shapefiles/test/neg
+
 # in ur workspace
 git clone -b noetic-devel https://github.com/tu-darmstadt-ros-pkg/hector_slam.git
 
